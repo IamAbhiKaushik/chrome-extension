9200b14e97211f58845a86b9d21c2cfb707e3e3b
IamAbhiKaushik <abhinavkaushik0898@gmail.com>
2019-09-15 13:54:29 +0530

    first version

---
+++ popup.js
@@ -0,0 +1,23 @@
+let changeColorR = document.getElementById('changeColorR');
+
+  chrome.storage.sync.get('color', function(data) {
+    changeColorR.style.backgroundColor = data.color;
+    changeColorR.setAttribute('value', data.color);
+  });
+  changeColorR.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+  changeColorW.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
